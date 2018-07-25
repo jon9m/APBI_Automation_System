fd9106656939239ad425f91e727bbb93174733d5
Vu Tuan <vutuan@Vus-iMac.local>
2018-07-25 15:50:41 +1000

    Navigation items init

--- src/components/Helpers/Navigation.js
+++ src/components/Helpers/Navigation.js
@@ -1,28 +1,118 @@
 import React, { Component } from 'react';
-
 import { NavLink } from 'react-router-dom';
 
+import Aux from '../../Hoc/Auxi';
+
 export default class Navigation extends Component {
+
+    isLinkActive = () => {
+        return false;
+    }
+
+    menuExpandedItems = [];
+
+    menuItemClickHandler = (itemId) => {
+        this.menuExpandedItems = [];
+        this.menuExpandedItems.push(itemId);
+    }
+
+    subMenuItemDisplayHandler = (subItemName) => {
+        if (this.menuExpandedItems.indexOf(subItemName) >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    navigationItems = [
+        {
+            type: "text",
+            level: 1,
+            id: "main_dashboard",
+            title: "Dashboard",
+            path: "/",
+            children: [
+                {
+                    type: "text",
+                    level: 2,
+                    id: "main_dashboard",
+                    title: "Main Dashboard",
+                    path: "/"
+                }
+            ]
+        },
+        {
+            type: "text",
+            level: 1,
+            id: "portal_setting",
+            title: "Portal Setting",
+            path: "/headquarters",
+            children: [
+                {
+                    type: "text",
+                    level: 2,
+                    id: "portal_setting",
+                    title: "Websites",
+                    path: "/"
+                },
+                {
+                    type: "text",
+                    level: 2,
+                    id: "portal_setting",
+                    title: "Social Media Accounts",
+                    path: "/"
+                }
+            ]
+        }
+    ];
+
     render() {
-        return(
+        const navItems = Object.keys(this.navigationItems).map((igKey) => {
+            return <Aux key={igKey}>
+                <li onClick={() => this.menuItemClickHandler(this.navigationItems[igKey].id)}>
+                    <NavLink isActive={this.isLinkActive} to={this.navigationItems[igKey].path} exact={true} activeClassName="active">
+                        <i className="md-icon">dashboard</i> <span>{this.navigationItems[igKey].title}</span>
+                    </NavLink>
+                </li>
+                {
+                    Object.keys(this.navigationItems[igKey].children).map((cKey) => {
+                        return this.subMenuItemDisplayHandler(this.navigationItems[igKey].children[cKey].id) ?
+                            <li key={cKey}>
+                                <NavLink to="/" exact={true} activeClassName="active">
+                                    <i className="sm-icon">dashboard</i> <span>{this.navigationItems[igKey].children[cKey].title}</span>
+                                </NavLink>
+                            </li>
+                            : null
+                    })
+                }
+            </Aux>
+        });
+
+
+        return (
             <div className="navigation">
                 <ul>
-                    <li>
-                        <NavLink to="/" exact={true} activeClassName="active">
-                             <i className="md-icon">dashboard</i> <span>Dashboard</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/" exact={true} activeClassName="active">
-                             <i className="sm-icon">dashboard</i> <span>Dashboard</span>
-                        </NavLink>
-                    </li>
+                    {navItems}
 
-                    <li>
+                    {/* <li onClick={() => this.menuItemClickHandler("main_dashboard")}>
+                        <NavLink isActive={this.isLinkActive} to="/" exact={true} activeClassName="active">
+                            <i className="md-icon">dashboard</i> <span>Dashboard</span>
+                        </NavLink>
+                    </li>
+                    {
+                        this.subMenuItemDisplayHandler("main_dashboard") ?
+                            <li>
+                                <NavLink to="/" exact={true} activeClassName="active">
+                                    <i className="sm-icon">dashboard</i> <span>Main Dashboard</span>
+                                </NavLink>
+                            </li>
+                            : null
+                    } */}
+
+                    {/* <li>
                         <NavLink to="/headquarters" activeClassName="active">
                             <i className="md-icon">settings</i> <span>Portal Setting</span>
                         </NavLink>
-                    </li>
+                    </li> */}
 
                     <li>
                         <NavLink to="/contacts" activeClassName="active">
